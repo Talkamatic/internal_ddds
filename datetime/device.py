f53c37ca24c5f547a123268955fdb0bade03747d
Alex Berman <alex@talkamatic.se>
2019-06-27 15:30:37 +0200

    Add test for passing datetime as grammar entry
    This change adds an interaction test in the 'datetime' DDD for
    passing datetime as a grammar entry to a service.
    
    Taiga issue #1641
    
    Change-Id: Ib88f91cce6f9f0627cd2313de071767df4768aa8

--- datetime/device.py
+++ datetime/device.py
@@ -9,3 +9,13 @@ class DatetimeDevice(DddDevice):
     class next_arrival_time(DeviceWHQuery):
         def perform(self):
             return ["2018-04-11T22:00:00.000Z"]
+
+    class event_time(DeviceWHQuery):
+        def perform(self):
+            return ["2018-05-06T09:30:00.000Z"]
+
+    class ShareEventTime(DeviceAction):
+        def perform(self, event_time):
+            def has_natural_language_form(date_time_expression):
+                return date_time_expression == "05/06/2018 09:30 AM"
+            return has_natural_language_form(event_time)
