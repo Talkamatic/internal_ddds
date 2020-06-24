fffa3ac26ad671576f1b593374c1c71217b94337
fkronlid <fredrik@talkamatic.se>
2020-06-24 14:07:10 +0200

    WIP: Negdial Phone Directory & MockupTravel updates
    Making sure that sequences of KPQs are also testable.
    
    Adding NLG component to phone_directory.
    
    Change-Id: I3ad456dd0f33d1f7ec13236612a0ba86268fbf67

---
+++ phone_directory/nlg/src/test/test_nl_generator.py
@@ -0,0 +1,34 @@
+import pytest
+from src.nl_generator import NLGenerator, FailedToGenerateMoveSequence
+
+
+class TestNLGenerator(object):
+    def setup(self):
+        self._generator = None
+        self._result = None
+
+    def _when_generating(self, moves):
+        self._moves = moves
+        self._given_generator()
+        self._generate_and_store_result()
+
+    def _given_generator(self):
+        self._generator = NLGenerator()
+
+    def _generate_and_store_result(self):
+        self._result = self._generator.generate(self._moves)
+
+    def _then_result_is(self, expected):
+        assert expected == self._result
+
+    def test_generate_utterance_for_city_kpq(self):
+        self._when_generating(["?know_answer(?X.person_city(X))"])
+        self._then_result_is("Vet du i vilken stad personen bor?")
+
+    def test_exception_when_failed_to_generate_move(self):
+        self._given_generator()
+        self._when_fail_to_generate_nl_exception_is_raised(["unexpected_move"], FailedToGenerateMoveSequence)
+
+    def _when_fail_to_generate_nl_exception_is_raised(self, moves, expected_exception):
+        with pytest.raises(expected_exception):
+            self._generator.generate(moves)
