0051a57f3c50d04d23f528a86b7ce024126e58ce
Amelie Åstbom <amelie@talkamatic.se>
2020-08-13 16:21:50 +0200

    Epic #1714: Intelligent feature question choice
    This change adds support for testing the
    "Intelligent feature question choice" feature,
    which means that the system does not ask about
    a feature if all alternatives have the same one.
    E.g. if all phone directory hits are in Gothenburg,
    the system does not ask about which city, but
    continues to another feature question.
    
    This change is related to the TDM change 1670:
    http://gerrit.talkamatic.se:8080/c/tdm/+/1670
    
    This change moves the old change with the same name
    in the phone_directory ddd from tdm/ddds to
    the internal_ddds submodule.
    
    Change-Id: Ie61923ae83828d9aefb7e5ff854aa81d40062485

--- phone_directory/http_service/http_service.py
+++ phone_directory/http_service/http_service.py
@@ -1,4 +1,8 @@
+# coding: utf-8
+
 import json
+import traceback
+import sys
 
 from flask import Flask, request
 from jinja2 import Environment
@@ -39,21 +43,41 @@ def person():
     try:
         payload = request.get_json()
         name_dict = payload["request"]["parameters"]["person_name"]
-        result = []
         name = name_dict["value"]
+        city_dict = payload["request"]["parameters"]["person_city"]
+        city_is_known = (city_dict is not None)
+        if city_is_known:
+            city = city_dict["value"]
+        result = []
         if name == "arne_osvaldsson":
             result.append({"value": "person_arne_osvaldsson",
                            "sort": "person_id",
                            "grammar_entry": "Arne Osvaldsson"})
         elif name == "susanna_andersson":
-            result.append({"value": "person_susanna_andersson_1",
+            if not city_is_known or city == "goteborg":
+                result.append({"value": "person_susanna_andersson_1",
+                               "sort": "person_id",
+                               "grammar_entry": "Susanna Andersson"})
+                result.append({"value": "person_susanna_andersson_2",
+                               "sort": "person_id",
+                               "grammar_entry": "Susanna Andersson"})
+            if not city_is_known or city == "stockholm":
+                result.append({"value": "person_susanna_andersson_3",
+                               "sort": "person_id",
+                               "grammar_entry": "Susanna Andersson"})
+        elif name == "ada_kallesson":
+            result.append({"value": "person_ada_kallesson_1",
                            "sort": "person_id",
-                           "grammar_entry": "Susanna Andersson"})
-            result.append({"value": "person_susanna_andersson_2",
+                           "grammar_entry": u"Ada Kållesson"})
+            result.append({"value": "person_ada_kallesson_2",
                            "sort": "person_id",
-                           "grammar_entry": "Susanna Andersson"})
+                           "grammar_entry": u"Ada Kållesson"})
         return person_response(result)
     except BaseException as exception:
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        error_string = "".join(traceback.format_exception(
+            exc_type, exc_value, exc_traceback))
+        print(error_string)
         return error_response(message=str(exception))
 
 
@@ -91,6 +115,8 @@ def _get_age(person):
         return 31
     elif person == "person_susanna_andersson_2":
         return 42
+    elif person == "person_ada_kallesson_2":
+        return 99
 
 
 @app.route("/age", methods=['POST'])
@@ -122,6 +148,57 @@ def age_response(age):
     }
      """)
     payload = response_template.render(age=age)
+    response = app.response_class(
+        response=payload,
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+def _get_city(person):
+    if person == "person_arne_osvaldsson":
+        return "goteborg"
+    elif person == "person_susanna_andersson_1":
+        return "goteborg"
+    elif person == "person_susanna_andersson_2":
+        return "goteborg"
+    elif person == "person_susanna_andersson_3":
+        return "stockholm"
+    elif person == "person_ada_kallesson_1":
+        return "goteborg"
+    elif person == "person_ada_kallesson_2":
+        return "goteborg"
+
+
+@app.route("/city", methods=['POST'])
+def city():
+    try:
+        payload = request.get_json()
+        person_dict = payload["request"]["parameters"]["person"]
+        person = person_dict["value"]
+        city = _get_city(person)
+        return city_response(city)
+    except BaseException as exception:
+        return error_response(message=str(exception))
+
+
+def city_response(city):
+    response_template = environment.from_string("""
+    {
+      "status": "success",
+      "data": {
+        "version": "1.0",
+        "result": [
+          {
+            "value": {{city|json}},
+            "confidence": 1.0,
+            "grammar_entry": null
+          }
+        ]
+      }
+    }
+     """)
+    payload = response_template.render(city=city)
     response = app.response_class(
         response=payload,
         status=200,
