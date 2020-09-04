efeadf92fd281650db01bc53480a04f2744dc02c
fkronlid <fredrik@talkamatic.se>
2020-09-04 11:32:32 +0200

    Apply Yapf
    Applied Yapf to previous_commitments/device.py
    
    Change-Id: I57e26298996131e31f414819166c31181a3b91e6

--- previous_commitments/device.py
+++ previous_commitments/device.py
@@ -2,8 +2,6 @@ from tala.model.device import DddDevice, DeviceWHQuery
 
 
 class PreviousCommitmentsDevice(DddDevice):
-
-
     class monthly_payment(DeviceWHQuery):
         def perform(self, interest_rate, mortgage_time, mortgage_sum):
             return [2000]
